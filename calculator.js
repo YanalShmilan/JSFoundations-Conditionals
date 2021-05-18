1ef8a56eaaa7e2d086fd5b5b84ab677f1826c4ab
yanalshmilan <yshmilan@gmail.com>
2021-05-18 10:14:31 +0300

    i hope it's the last edit

--- calculator.js
+++ calculator.js
@@ -2,14 +2,18 @@ const prompt = require('prompt-sync')(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const a = parseInt(prompt('enter num 1: '));
-const b = parseInt(prompt('enter num 2: '));
+const a = prompt('enter num 1: ');
+const b = prompt('enter num 2: ');
 const c = prompt('operation: ');
-if (c === '+') console.log(a + b);
-if (c === '-') console.log(a - b);
-if (c === '*') console.log(a * b);
-if (c === '/') console.log(a / b);
-
-const laila = prompt('which laila are you (A or B)');
-if (laila === 'A');
-console.log('You are the best Laila!');
+if (isNaN(a) || isNaN(b)) {
+  console.log('invalid input');
+} else {
+  if (c !== '+' && c !== '-' && c !== '+' && c !== '*' && c !== '/') {
+    console.log('operation is not valid');
+  } else {
+    if (c === '+') console.log(parseInt(a) + parseInt(b));
+    if (c === '-') console.log(a - b);
+    if (c === '*') console.log(a * b);
+    if (c === '/') console.log(a / b);
+  }
+}
