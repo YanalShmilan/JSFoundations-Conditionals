69071477a5c56eb89fbc7a3bc62f8587e20eab50
yanalshmilan <yshmilan@gmail.com>
2021-05-18 10:22:35 +0300

    done

--- calculator.js
+++ calculator.js
@@ -8,12 +8,12 @@ const c = prompt('operation: ');
 if (isNaN(a) || isNaN(b)) {
   console.log('invalid input');
 } else {
-  if (c !== '+' && c !== '-' && c !== '+' && c !== '*' && c !== '/') {
+  if (c !== '+' && c !== '-' && c !== '-' && c !== '*' && c !== '/') {
     console.log('operation is not valid');
   } else {
     if (c === '+') console.log(parseInt(a) + parseInt(b));
-    if (c === '+') console.log(parseInt(a) - parseInt(b));
-    if (c === '+') console.log(parseInt(a) * parseInt(b));
-    if (c === '+') console.log(parseInt(a) / parseInt(b));
+    if (c === '-') console.log(parseInt(a) - parseInt(b));
+    if (c === '*') console.log(parseInt(a) * parseInt(b));
+    if (c === '/') console.log(parseInt(a) / parseInt(b));
   }
 }
